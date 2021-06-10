22a7fab0558e0ae7ff6a25a2099d49679cbbe919
DanielZitoli <danielzitoli@rogers.com>
2021-06-09 22:15:07 -0400

    Finished all styles. Starting work on js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", function(){
-    let gridWidth = 16;
+    let gridWidth = 30;
     let container = document.querySelector(".container");
 
     setInitialGrid();
@@ -30,25 +30,34 @@ addEventListener("DOMContentLoaded", function(){
             let pixel = document.createElement("div");
             container.append(pixel);
         }
-    }
+    };  
 
-
-
-
-    function setGrid(width)
+    function setGrid(gridWidth)
     {
+        setWidthandBorder(gridWidth);
 
-        let divWidth = 500 / width;
-        document.documentElement.style.setProperty('--pixel-width', divWidth + "px");
-        
         let pixels = document.querySelectorAll('div.container > div');
 
-        for (let i = 0; i < width**2; i++){
+        for (let i = 0; i < gridWidth**2; i++){
             pixels[i].classList.add("pixel");
         }
-        for (let i = width**2; i < 100**2; i++){
+        for (let i = gridWidth**2; i < 100**2; i++){
             pixels[i].removeAttribute('class');
         }
+    };
+
+    function setWidthandBorder(gridWidth){
+        let borderWidth = 1;
+        let borderColor = 'black';
+
+        if (gridWidth > 64){
+            borderWidth = 0;
+        }
+
+        let pixelWidth = 500 / gridWidth - 2 * borderWidth;
+        document.documentElement.style.setProperty('--pixel-width', pixelWidth + "px");
+        document.documentElement.style.setProperty('--border-width', borderWidth + "px");
+        document.documentElement.style.setProperty('--border-color', borderColor);
     };
 
 });
